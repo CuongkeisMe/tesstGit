356dc1092f8f53fee61bf928469337a189f50bfc
CuongkeisMe <nqc23082005@gmail.com>
2025-05-31 11:02:30 +0700

    cuong v2

--- src/main/java/com/example/demo/Repository/OrdersRepository.java
+++ src/main/java/com/example/demo/Repository/OrdersRepository.java
@@ -2,6 +2,9 @@ package com.example.demo.Repository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class OrdersRepository {
+    private List<Order> orders;    
 }
